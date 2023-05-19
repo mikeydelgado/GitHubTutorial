5f2bcf3a7b22a849c788bd278df963ccbd5de2a9
Mikey Delgado <mikeyadelgadojr@gmail.com>
2023-05-19 17:41:47 -0400

    I added another line of code.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld{
   public static void main(String[] args){
-   System.out.print("Lol doing hello world is so stupid"); 
+   System.out.print("Lol doing hello world is so stupid");
+   System.out.print("this is a new line of code");
   }
   
 }
